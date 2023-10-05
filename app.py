b7e519522c7b1e7156a10b4cd30b15bc48689df1
Seanghay Yath <seanghay.dev@gmail.com>
2023-10-05 16:47:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ net_g = SynthesizerTrn(
 )
 
 _ = net_g.eval()
-_ = utils.load_checkpoint("G_35000.pth", net_g, None)
+_ = utils.load_checkpoint("G_60000.pth", net_g, None)
 
 def generate_voice(text):
     text = khmernormalizer.normalize(text)
